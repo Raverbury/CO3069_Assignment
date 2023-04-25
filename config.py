5bff17300fd583882680b26d0398bf4aacdee887
Raverbury <smurfjp198@gmail.com>
2023-04-25 22:38:01 +0700

    reindex

--- config.py
+++
@@ -1,9 +0,0 @@
-sender_email = "qwertydefacecheck@gmail.com"
-sender_password = "hreglqhofoqvjvnx"
-
-recipient_email = "stormraider198@gmail.com"
-
-scan_interval = 1 # in minutes
-
-blacklist = [r".git"]
-dir_path = r"C:\xampp\htdocs\jobseeker"
